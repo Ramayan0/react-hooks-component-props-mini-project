c969070a16e55ccdb3b7b194123bb153bccc96ca
hamsa <hafieekhan@gmail.com>
2022-09-22 00:43:56 +0300

    "Completed Lab"

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About({ about, img = "https://via.placeholder.com/215" }) {
+  return (
+    <aside>
+      <img src={img} alt="blog logo"></img>
+      <p>{about}</p>
+    </aside>
+  );
+}
+export default About;
